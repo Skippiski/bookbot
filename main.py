da9267ba5405859924b5ba3180c8b70e18552a70
Skippiski <zachosowski@yahoo.com>
2025-03-19 16:40:45 -0700

    adjusted main.py to include wordcount

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def get_book_text(filepath):
 
     return file_contents
 
+def num_words(file_contents):
+    words = file_contents.split()
+    return len(words)
+
 def main ():
     filepath = "books/frankenstein.txt"
     book_text = get_book_text(filepath)
-    print(book_text)
+    total_words = num_words(book_text)
+    print(book_text,f"{total_words} words found in the document.")
 
 
 main()
