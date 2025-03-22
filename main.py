f3d555122b91b2f92ffa9a51ad054186c4984f63
Skippiski <zachosowski@yahoo.com>
2025-03-22 16:13:10 -0700

    added code for sys functions to allow for terminal commands

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-     file_contents = f.read()
-
-    return file_contents
+import sys
 
 from stats import num_words, letter_count,sort_on, sortCL
 
+def get_book_text(filepath):
+     with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
 
 def main ():
-    filepath = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+       print("Usage: python3 main.py <path_to_book>")
+       sys.exit(1)
+    
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     num_words1 = num_words(book_text)
     letter_counts= letter_count(book_text)
     sorted_chars = sortCL(letter_counts)
-
+    
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
